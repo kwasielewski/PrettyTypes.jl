5c7b471824a75f85a6e1798e2826fd5eba453db2
Krzysztof Wasielewski <krzychuwas@gmail.com>
2024-05-09 09:15:20 +0200

    Added exception handling to keep repl running on pretty printing errors

--- src/PrettyTypes.jl
+++ src/PrettyTypes.jl
@@ -18,7 +18,7 @@ macro type(exp)
                 elseif mtds[1].sig isa UnionAll
                     push!(constraints, mtds[1].sig.body.parameters[2])
                     signature = mtds[1].sig.body.parameters[2:end]
-					get_or_id = x -> hasproperty(x, :name) ? x.name : x
+                    get_or_id = x -> hasproperty(x, :name) ? x.name : x
                     print_signature = get_or_id.(signature)
                 end
 
@@ -28,25 +28,25 @@ macro type(exp)
                     print_signature = ["()"]
                 end
                 if length(rettp) == 1
-					has_union = any(x -> x isa Union, print_signature)
-					has_tvar = any(x -> x isa Symbol, print_signature)
+                    has_union = any(x -> x isa Union, print_signature)
+                    has_tvar = any(x -> x isa Symbol, print_signature)
                     if length(constraints) > 0 && !has_union
                         print("[", join(constraints, ", "), "] => ")
                     end
-					if has_union && !has_tvar
-						println("{")
-						print_signature = map(x -> union_types(x), print_signature)
-						for t in Iterators.product(print_signature...)
-							rettp = Base.return_types($(esc(exp)), t)
-							if length(rettp) != 1
-								@warn "Multiple return types for signature: ", join(t, " * ")
-							end
-							println(join(t, " * "), " -> ", rettp[1])
-						end
-						println("}")
-					elseif !has_union
-                    	println(join(print_signature, " * "), " -> ", rettp[1])
-					end
+                    if has_union && !has_tvar
+                        println("{")
+                        print_signature = map(x -> union_types(x), print_signature)
+                        for t in Iterators.product(print_signature...)
+                            rettp = Base.return_types($(esc(exp)), t)
+                            if length(rettp) != 1
+                                @warn "Multiple return types for signature: ", join(t, " * ")
+                            end
+                            println(join(t, " * "), " -> ", rettp[1])
+                        end
+                        println("}")
+                    elseif !has_union
+                        println(join(print_signature, " * "), " -> ", rettp[1])
+                    end
                 end
             end
         end
@@ -55,9 +55,20 @@ macro type(exp)
 end
 
 function ast_transform(e::Expr, m::Module)
-    return Expr(:block, quote
-            (macroexpand($m, @type macroexpand($m, $e)))
-        end, e)
+    return :(
+        begin
+            try
+                macroexpand($m, @type macroexpand($m, $e))
+            catch exc
+                trace = stacktrace(Base.catch_backtrace())
+                idx = findfirst(x -> occursin("PrettyTypes.jl", string(x.file)), trace)
+                @warn("Error in type pretty printing: ",
+                    exc,
+                    _line = trace[idx].line,
+                    _file = string(trace[idx].file))
+            end
+            $e
+        end)
 end
 
 end # module PrettyTypes
